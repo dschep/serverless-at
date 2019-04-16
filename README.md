62c1fad36d09b727663368d0749c1bca0affc992
Eslam A. Hefnawy <eahefnawy@gmail.com>
2019-04-16 14:54:29 +0300

    published 0.1.0

---
+++ README.md
@@ -0,0 +1,113 @@
+# Socket
+
+&nbsp;
+
+Instantly run scheduled cron jobs on AWS using [Serverless Components](https://github.com/serverless/components).
+
+&nbsp;
+
+1. [Install](#1-install)
+2. [Create](#2-create)
+3. [Configure](#3-configure)
+4. [Deploy](#4-deploy)
+
+&nbsp;
+
+
+### 1. Install
+
+```console
+$ npm install -g @serverless/components
+```
+
+### 2. Create
+
+```console
+$ mkdir cron-job && cd cron-job
+```
+
+The directory should look something like this:
+
+
+```
+|- cron.js
+|- serverless.yml
+|- package.json # optional
+|- .env         # your development AWS api keys
+|- .env.prod    # your production AWS api keys
+```
+
+the `.env` files are not required if you have the aws keys set globally and you want to use a single stage, but they should look like this.
+
+```
+AWS_ACCESS_KEY_ID=XXX
+AWS_SECRET_ACCESS_KEY=XXX
+```
+
+The `cron.js` file contains your cron code.
+
+```js
+module.exports.handler = async (e, ctx, cb) => {
+  console.log('running in schedule')
+}
+```
+
+### 3. Configure
+
+All the following inputs are optional. However, they allow you to configure your Lambda compute instance and pass environment variables.
+
+```yml
+# serverless.yml
+
+name: cron-job
+stage: dev
+
+Cron:
+  component: "@serverless/cron"
+  inputs:
+    name: cron-job
+    
+    # you can provide a rate either as rate with
+    # this format <amount><unit-character> (e.g. 1s, 5m, 2h)
+    # or a cron expresion  
+    rate: 5m 
+    enabled: true # this is the default value
+    description: My Cron Job
+    regoin: us-east-1
+    memory: 128
+    timeout: 10
+    env:
+      TABLE_NAME: my-table
+    
+    # the directory that contains the cron.js file.
+    # If not provided, the default is the current working directory
+    code: ./code
+
+
+```
+
+### 4. Deploy
+
+```console
+Socket (master)$ components
+
+  Socket › outputs:
+  url:  'wss://3v1fypmyz8.execute-api.us-east-1.amazonaws.com/dev/'
+  code: 
+    runtime:  'nodejs8.10'
+    env:  []
+    timeout:  10
+    memory:  512
+  routes:  [ '$connect', '$disconnect', '$default' ]
+
+
+  36s › dev › Socket › done
+
+Socket (master)$
+```
+
+&nbsp;
+
+### New to Components?
+
+Checkout the [Serverless Components](https://github.com/serverless/components) repo for more information.
---
+++ serverless.js
@@ -0,0 +1,115 @@
+const { Component } = require('@serverless/components')
+const AWS = require('aws-sdk')
+
+const parseRate = (rate = '1m') => {
+  const unit = rate.substr(rate.length - 1)
+  if (['m', 'h', 'd'].includes(unit)) {
+    let awsUnit
+    const period = rate.substr(0, rate.length - 1)
+    if (period === '1') {
+      if (unit === 'm') {
+        awsUnit = 'minute'
+      }
+      if (unit === 'h') {
+        awsUnit = 'hour'
+      }
+      if (unit === 'd') {
+        awsUnit = 'day'
+      }
+    } else {
+      if (unit === 'm') {
+        awsUnit = 'minutes'
+      }
+      if (unit === 'h') {
+        awsUnit = 'hours'
+      }
+      if (unit === 'd') {
+        awsUnit = 'days'
+      }
+    }
+    return `rate(${period} ${awsUnit})`
+  }
+  return `cron(${rate})`
+}
+
+class Cron extends Component {
+  async default(inputs = {}) {
+    this.cli.status('Deploying')
+    const awsLambda = await this.load('@serverless/aws-lambda')
+
+    inputs.name =
+      inputs.name ||
+      this.state.name ||
+      `cron-${Math.random()
+        .toString(36)
+        .substring(6)}`
+    inputs.handler = inputs.handler || 'cron.handler'
+    inputs.parsedRate = parseRate(inputs.rate || '1m')
+    inputs.enabled = inputs.enabled || true
+
+    const lambdaOutput = await awsLambda(inputs)
+
+    const lambda = new AWS.Lambda({
+      region: 'us-east-1',
+      credentials: this.context.credentials.aws
+    })
+    const cloudWatchEvents = new AWS.CloudWatchEvents({
+      region: 'us-east-1',
+      credentials: this.context.credentials.aws
+    })
+
+    const putRuleParams = {
+      Name: inputs.name,
+      ScheduleExpression: inputs.parsedRate,
+      State: inputs.enabled ? 'ENABLED' : 'DISABLED'
+    }
+
+    await cloudWatchEvents.putRule(putRuleParams).promise()
+
+    const putTargetsParams = {
+      Rule: inputs.name,
+      Targets: [
+        {
+          Arn: lambdaOutput.arn,
+          Id: inputs.name
+        }
+      ]
+    }
+
+    await cloudWatchEvents.putTargets(putTargetsParams).promise()
+
+    const addPermissionParams = {
+      Action: 'lambda:InvokeFunction',
+      FunctionName: inputs.name,
+      StatementId: `cron-${inputs.name}`,
+      Principal: 'events.amazonaws.com'
+    }
+
+    try {
+      await lambda.addPermission(addPermissionParams).promise()
+    } catch (e) {
+      // if we are making an update, permissions are already added...
+      if (e.code !== 'ResourceConflictException') {
+        throw e
+      }
+    }
+
+    this.state.name = inputs.name
+    await this.save()
+
+    return { ...lambdaOutput, rate: inputs.rate || '1m', enabled: inputs.enabled }
+  }
+
+  async remove() {
+    this.cli.status('Deploying')
+    const awsLambda = await this.load('@serverless/aws-lambda')
+
+    await awsLambda.remove()
+
+    this.state = {}
+    await this.save()
+    return {}
+  }
+}
+
+module.exports = Cron
